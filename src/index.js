d22440eed4f93a90a2cb7e37cdaf96ec59699435
Gil Jacob Abadi <giljabadi@gmail.com>
2022-11-08 11:10:12 -0600

    complete seasons react project

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./loader";
+
+
 // ---------------------------------------------
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {lat: null, errorMessage: ''};
-    };
+    state = {lat: null, errorMessage: ''};
 
     componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition((position) => {
-                this.setState({lat: position.coords.latitude})
-            }, err => {
-                this.setState({errorMessage: err.message})
-            }
+        window.navigator.geolocation.getCurrentPosition((position) => this.setState({lat: position.coords.latitude}), err => this.setState({errorMessage: err.message})
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage} </div>
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat} </div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
-        return <div>Loading!</div>
+        return <div><Spinner message="Please accept location request" />!</div>
+    }
+
+    render() {
+        return (
+            <div className='border'>
+                {this.renderContent()}
+            </div>
+        )
 
     }
 }
@@ -35,4 +38,3 @@ ReactDOM.render(
     <App/>,
     document.querySelector('#root')
 );
-
